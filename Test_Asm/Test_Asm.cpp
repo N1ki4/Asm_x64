37389fcb4d64e340be73f21f1880a644611a6948
m_hlazunov <glazu@HOME-PC>
2024-05-05 15:24:25 +0200

    Colors table implementation

--- Test_Asm/Test_Asm.cpp
+++ Test_Asm/Test_Asm.cpp
@@ -1,24 +1,24 @@
 #include <windows.h>
 #include <stdio.h>
 
+//------------------------------------------------------------------------------------------------------------
 struct SPos
 {
-    SPos(unsigned short x_pos, unsigned short y_pos, unsigned short screen_width, unsigned short len)
-        : X_Pos(x_pos), Y_Pos(y_pos), Screen_Width(screen_width), Len(len)
-    {
-    }
-    unsigned short X_Pos;
-    unsigned short Y_Pos;
-    unsigned short Screen_Width;
-    unsigned short Len;
+   SPos(unsigned short x_pos, unsigned short y_pos, unsigned short screen_width, unsigned short len)
+      : X_Pos(x_pos), Y_Pos(y_pos), Screen_Width(screen_width), Len(len)
+   {
+   }
 
+   unsigned short X_Pos;
+   unsigned short Y_Pos;
+   unsigned short Screen_Width;
+   unsigned short Len;
 };
-
-extern "C" int Make_Sum(int first_val, int second_val);
-extern "C" void Draw_Line_Horizontal(CHAR_INFO* screen_buffer, SPos pos, CHAR_INFO symbol);
-extern "C" void Show_Colors(CHAR_INFO * screen_buffer, SPos pos, CHAR_INFO symbol);
-
-
+//------------------------------------------------------------------------------------------------------------
+extern "C" int Make_Sum(int one_value, int another_value);
+extern "C" void Draw_Line_Horizontal(CHAR_INFO *screen_buffer, SPos pos, CHAR_INFO symbol);
+extern "C" void Show_Colors(CHAR_INFO *screen_buffer, SPos pos, CHAR_INFO symbol);
+//------------------------------------------------------------------------------------------------------------
 int main(void)
 {
     HANDLE std_handle, screen_buffer_handle;
@@ -29,7 +29,6 @@ int main(void)
     int screen_buffer_size;
 
     // Get a handle to the STDOUT screen buffer to copy from and create a new screen buffer to copy to.
-
     std_handle = GetStdHandle(STD_OUTPUT_HANDLE);
     screen_buffer_handle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CONSOLE_TEXTMODE_BUFFER, 0);
     if (std_handle == INVALID_HANDLE_VALUE || screen_buffer_handle == INVALID_HANDLE_VALUE)
@@ -38,17 +37,19 @@ int main(void)
         return 1;
     }
 
-    if (!SetConsoleActiveScreenBuffer(screen_buffer_handle))
+    // Make the new screen buffer the active screen buffer.
+    if (! SetConsoleActiveScreenBuffer(screen_buffer_handle) )
     {
         printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
         return 1;
     }
 
-    if (!GetConsoleScreenBufferInfo(screen_buffer_handle, &screen_buffer_info))
+    if (! GetConsoleScreenBufferInfo(screen_buffer_handle, &screen_buffer_info) )
     {
         printf("GetConsoleScreenBufferInfo failed - (%d)\n", GetLastError());
         return 1;
     }
+
     screen_buffer_size = (int)screen_buffer_info.dwSize.X * (int)screen_buffer_info.dwSize.Y;
     screen_buffer = new CHAR_INFO[screen_buffer_size];
     memset(screen_buffer, 0, screen_buffer_size * sizeof(CHAR_INFO));
@@ -60,32 +61,37 @@ int main(void)
     srctWriteRect.Bottom = 11; // bot. rt: row 11, col 79
     srctWriteRect.Right = 79;
 
+    // Copy from the temporary buffer to the new screen buffer.
+
+    //screen_buffer[0].Char.UnicodeChar = L'W';
+    //screen_buffer[0].Attributes = 0x50;
+
     CHAR_INFO symbol{};
-    symbol.Char.UnicodeChar = L'X';
-    symbol.Attributes = 0x50;
-    
+    symbol.Char.UnicodeChar = L'-';
+    symbol.Attributes = 0x1b;
+
     SPos pos(2, 1, screen_buffer_info.dwSize.X, 10);
 
-    // Draw_Line_Horizontal(screen_buffer, pos, symbol);
+    Draw_Line_Horizontal(screen_buffer, pos, symbol);
 
     Show_Colors(screen_buffer, pos, symbol);
 
-
-    if (!WriteConsoleOutput(screen_buffer_handle, screen_buffer, screen_buffer_info.dwSize, screen_buffer_pos, &srctWriteRect))
+    if (! WriteConsoleOutput(screen_buffer_handle, screen_buffer, screen_buffer_info.dwSize, screen_buffer_pos, &screen_buffer_info.srWindow) )
     {
         printf("WriteConsoleOutput failed - (%d)\n", GetLastError());
         return 1;
     }
-    Sleep(5000);
+
+    Sleep(150 * 1000);
 
     // Restore the original active screen buffer.
 
-    if (!SetConsoleActiveScreenBuffer(std_handle))
+    if (! SetConsoleActiveScreenBuffer(std_handle))
     {
         printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
         return 1;
     }
 
-
     return 0;
 }
+//------------------------------------------------------------------------------------------------------------
--- Test_Asm/asm_utils.asm
+++ Test_Asm/asm_utils.asm
@@ -1,10 +1,9 @@
-.code 
-;-------------------------------------------------------------------------------------------------------------------------------
+.code
+;-------------------------------------------------------------------------------------------------------------
 Make_Sum proc
-; int make_sum(int first_val, int second_val)
-; RCX - first_val
-; RDX - second_val
-; return RAX
+; int Make_Sum(int one_value, int another_value)
+; RCX - one_value
+; RDX - another_value
 
 	mov eax, ecx
 	add eax, edx
@@ -12,61 +11,56 @@ Make_Sum proc
 	ret
 
 Make_Sum endp
-;-------------------------------------------------------------------------------------------------------------------------------
-
+;-------------------------------------------------------------------------------------------------------------
 Get_Pos_Address proc
 ; RCX - screen_buffer
 ; RDX - pos
-; return RDI
 
-; 1. Calculate output address: address_offset = (pos.Y_Pos * pos.Screen_Width + pox.X_Pos) * 4
-
-; 1.1. Calculate pos.Y * pos.Screen_Width
+	; 1. Calculate addres_offset = (pos.Y_Pos * pos.Screen_Width + pos.X_Pos) * 4
+	; 1.1.Calculate pos.Y * pos.Screen_Width
 	mov rax, rdx
-	shr rax, 16    ; AX = pos.Y_Pos
-	movzx rax, ax  ; RAX = AX == pos.Y_pos
+	shr rax, 16  ; AX = pos.Y_Pos
+	movzx rax, ax  ; RAX = AX = pos.Y_Pos
 
 	mov rbx, rdx
-	shr rbx, 32    ; BX = pos.Screen_Width
-	movzx rbx, bx  ; RBX = BX == pos.Screen_Width
+	shr rbx, 32  ; BX = pos.Screen_Width
+	movzx rbx, bx  ; RBX = BX = pos.Screen_Width
 
-	imul rax, rbx  ; RAX = RAX * RBX == pos.Y_Pos * pos.Screen_Width
+	imul rax, rbx  ; RAX = RAX * RBX = pos.Y_Pos * pos.Screen_Width
 
-; 1.2. Add pos.X to RAX
-	movzx rbx, dx  ; RBX = DX = pos.X_Pos
-	add rax, rbx   ; RAX = pos.Y_Pos * pos.Screen_Width + pos.X_Pos
+	; 1.2. Add pos.X к RAX
+	movzx rbx, dx  ; RBX = DX = pox.X_Pos
+	add rax, rbx  ; RAX = pos.Y_Pos * pos.Screen_Width + pox.X_Pos = offset in characters
 
-; 1.3. RAX contains shift start of the string in bytes, but we need in bytes. 
-; Hence each symbol has 4 bytes, we need to multiply this shift on 4.
-	shl rax, 2     ; RAX = RAX * 4 = address_offset
+	; 1.3.  RAX contains the offset of the beginning of the line in characters, but it should be in bytes.
+	; Because each character takes 4 bytes, you need to multiply this offset by 4
+	shl rax, 2  ; RAX = RAX * 4 = addres_offset
 
-	mov rdi, rcx   ; RDI = screen_buffer
-	add rdi, rax   ; RDI = screen_buffer + address_offset
+	mov rdi, rcx  ; RDI = screen_buffer
+	add rdi, rax  ; RDI = screen_buffer + addres_offset
 
 	ret
 
 Get_Pos_Address endp
-;-------------------------------------------------------------------------------------------------------------------------------
-
+;-------------------------------------------------------------------------------------------------------------
 Draw_Line_Horizontal proc
-; void Draw_Line_Horizontal(CHAR_INFO* screen_buffer, SPos pos, CHAR_INFO symbol)
+; extern "C" void Draw_Line_Horizontal(CHAR_INFO *screen_buffer, SPos pos, CHAR_INFO symbol);
 ; RCX - screen_buffer
 ; RDX - pos
 ; R8 - symbol
-; return RAX
-	
+
 	push rax
 	push rbx
 	push rcx
 	push rdi
 
-; 1. Calculate output address
-	call Get_Pos_Address  ; RDI = symbol position in buffer_screen in position "pos"
+	; 1. Calculate the output address
+	call Get_Pos_Address  ; RDI = character position in buffer screen_buffer in positions pos
 
-; 2. Output symbols 
+	; 2. Outputting characters
 	mov eax, r8d
 	mov rcx, rdx
-	shr rcx, 48    ; RCX = CX = pos.Len
+	shr rcx, 48  ; RCX = CX = pos.Len
 
 	rep stosd
 
@@ -78,24 +72,74 @@ Draw_Line_Horizontal proc
 	ret
 
 Draw_Line_Horizontal endp
-;-------------------------------------------------------------------------------------------------------------------------------
-
+;-------------------------------------------------------------------------------------------------------------
 Show_Colors proc
-; void Show_Colors(CHAR_INFO * screen_buffer, SPos pos, CHAR_INFO symbol);
+; extern "C" void Show_Colors(CHAR_INFO *screen_buffer, SPos pos, CHAR_INFO symbol);
 ; RCX - screen_buffer
 ; RDX - pos
 ; R8 - symbol
-; return RAX
 
-; 1. Calculate output address
-	call Get_Pos_Address  ; RDI = symbol position in buffer_screen in position "pos"
-	
-	mov rax, r8
+	push rax
+	push rbx
+	push rcx
+	push rdi
+	push r10
+	push r11
 
+	; 1. Calculate the output address
+	call Get_Pos_Address  ; RDI = position character in buffer screen_buffer in positions pos
+
+	mov r10, rdi
+
+	; 2. Output Position Correction Calculation
+	mov r11, rdx
+	shr r11, 32  ; R11 = pos
+	movzx r11, r11w  ; R11 = R11W = pos.Screen_Width
+	shl r11, 2  ; R11 = pos.Screen_Width * 4 = Screen width in bytes
+
+	; 3. Preparing cycles
+	mov rax, r8  ; RAX = EAX = symbol
+
+	; 3.1. Result of the AND command (logical "AND")
+	; 0 & 0 = 0
+	; 0 & 1 = 0
+	; 1 & 0 = 0
+	; 1 & 1 = 1
+
+	and rax, 0ffffh  ; Reset all RAX bytes except 0 and 1
+	mov rbx, 16
+
+	xor rcx, rcx  ; RCX = 0
+
+	; 0 XOR 0 = 0
+	; 0 XOR 1 = 1
+	; 1 XOR 0 = 1
+	; 1 XOR 1 = 0
+
+_0:
+	mov cl, 16
+
+_1:
 	stosd
+	add rax, 010000h  ; A unit shifted 16 bits to the left (i.e. the elementary step for attributes)
+
+	loop _1
+
+	add r10, r11
+	mov rdi, r10
+
+	dec rbx
+	jnz _0
+
+	pop r11
+	pop r10
+	pop rdi
+	pop rcx
+	pop rbx
+	pop rax
 
 	ret
 
 Show_Colors endp
-;-------------------------------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------------
 end
